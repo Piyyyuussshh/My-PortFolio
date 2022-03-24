f38ea5b8a4548fbac3214cd465d2f2556418820b
Piyush Mehra <piyush.mehra13@gmail.com>
2022-03-24 15:30:22 +0530

    Added Functionality to each button.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const secBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections= document.querySelectorAll('.main-content');
+const allSections= document.querySelector('.main-content');
 
 function PageTransitions(){
     //Button click active class
@@ -9,9 +9,28 @@ function PageTransitions(){
         sectBtn[i].addEventListener('click',function(){
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
-            this.className += 'active-btn' 
+            this.className += ' active-btn' 
         })
     }
+    //Section Active Class
+    allSections.addEventListener('click',(e)=>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from other btns
+            secBtns.forEach((btn)=>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id)
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
